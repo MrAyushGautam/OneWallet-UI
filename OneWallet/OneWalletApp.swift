b53f97809916f2bdcbce74536f03c571aed4327b
MrAyushGautam <info.ayushgautam@gmail.com>
2024-08-09 10:45:18 +0530

    Initial Commit

---
+++ OneWallet/OneWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneWalletApp.swift
+//  OneWallet
+//
+//  Created by Morning Star on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OneWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
